cc6f1831b21dbe457d38e9cabdc3d3d86cd5f509
Amanda-Lei <amandaylei@gmail.com>
2024-01-03 21:27:57 -0500

    multi item carousel
    small fixes. and getting multiple images to show again but within js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
     // later might make the array pull all files from a folder
     var imageArray = [
-        'gallery.jpg',
-        'gallery.jpg',
-        'gallery.jpg',
+        'images/gallery.jpg',
+        'images/gallery.jpg',
+        'images/gallery.jpg',
+        'images/gallery.jpg'
     ];
 
     var carouselInner = document.querySelector('#techshare-gallery > .carousel-inner');
@@ -11,17 +12,26 @@ document.addEventListener('DOMContentLoaded', function () {
     // add each image as a carousel item
     imageArray.forEach(function (imageSrc, index) {
         var carouselItem = document.createElement('div');
-        carouselItem.classList.add('carousel-item');
+        carouselItem.classList.add('carousel-item', 'row');
 
+        //set first item active
         if (index === 0) {
-            carouselItem.classList.add('active'); // set the first item as active
+            carouselItem.classList.add('active');
         }
 
+        //create col div and card div
+        var column = document.createElement('div').classList.add('col-sm-3');
+        var card = document.createElement('div').classList.add('card');
+
+        // image
         var img = document.createElement('img');
         img.src = imageSrc;
-        img.classList.add('d-block', 'w-100'); // bs for responsive images
+        img.classList.add('img-fluid');
 
-        carouselItem.appendChild(img);
+        //nest classes
+        card.appendChild(img);
+        column.appendChild(card);
+        carouselItem.appendChild(column);
         carouselInner.appendChild(carouselItem);
     });
 });
